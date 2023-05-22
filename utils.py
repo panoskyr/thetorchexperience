17cc0b6b0621cffee18ef5e4ea0f4f1a14a1f900
panoskyr <35936229+panoskyr@users.noreply.github.com>
2023-05-22 15:29:22 +0300

    some add

---
+++ utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from skimage.io import imread
+
+
+def show_image(image):
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+
+def show_image_w_label(image, label,cmap='gray'):
+    plt.imshow(image, cmap=cmap)
+    plt.axis('off')
+    plt.title(label.map({0: 'Negative', 1: 'Positive'}))
+    plt.show()
